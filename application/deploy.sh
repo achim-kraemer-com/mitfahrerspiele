fb4a7a51cf72cb8fc9c93496136f9e31c3992642
Achim Krämer <72191461+achim-kraemer@users.noreply.github.com>
2020-12-20 19:50:23 +0100

    m2: daploy.sh changed

--- application/deploy.sh
+++ application/deploy.sh
@@ -1,3 +1,6 @@
-php74 composer.phar install
-php74 bin/console doctrine:migrations:migrate -n
-php74 bin/console cache:clear
+composer self-update
+composer install
+php bin/console doctrine:migrations:migrate -n
+php bin/console cache:clear
+yarn install
+yarn encore production
